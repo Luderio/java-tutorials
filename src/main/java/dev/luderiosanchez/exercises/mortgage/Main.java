e13678db14bfd7bfe86050b6f29a394c17f9bff4
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2026-05-25 11:36:04 +0800

    Code Update: Updated the logger on the main class

--- src/main/java/dev/luderiosanchez/exercises/mortgage/Main.java
+++ src/main/java/dev/luderiosanchez/exercises/mortgage/Main.java
@@ -3,7 +3,6 @@ package dev.luderiosanchez.exercises.mortgage;
 public class Main {
     public static void main(String[] args) {
         String monthlyPayments = MortgageCalculator.getMonthlyPayments();
-
-        System.out.println(monthlyPayments);
+        MortgageUtils.log(MortgageUtils.LogType.INFO, monthlyPayments);
     }
 }
