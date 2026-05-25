003e613f564b6e8da9b05ee573ed1c132d3c83fe
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2026-05-25 15:05:55 +0800

    Code Update: Added a new method that calculates and outputs the payment schedule

--- src/main/java/dev/luderiosanchez/exercises/mortgage/Main.java
+++ src/main/java/dev/luderiosanchez/exercises/mortgage/Main.java
@@ -3,6 +3,6 @@ package dev.luderiosanchez.exercises.mortgage;
 public class Main {
     public static void main(String[] args) {
         String monthlyPayments = MortgageCalculator.getMonthlyPayments();
-        MortgageUtils.log(MortgageUtils.LogType.INFO, monthlyPayments);
+        System.out.println(monthlyPayments);
     }
 }
--- src/main/java/dev/luderiosanchez/exercises/mortgage/MortgageCalculator.java
+++ src/main/java/dev/luderiosanchez/exercises/mortgage/MortgageCalculator.java
@@ -1,5 +1,6 @@
 package dev.luderiosanchez.exercises.mortgage;
 
+import java.util.ArrayList;
 import java.util.Locale;
 import java.text.NumberFormat;
 import java.util.Scanner;
@@ -17,15 +18,50 @@ public class MortgageCalculator {
 
     public static String getMonthlyPayments() {
         System.out.println("Mortgage Calculator");
+        System.out.println("------------------------------------");
         Scanner scanner = new Scanner(System.in);
 
         double principal = (double) MortgageUtils.inputValidator(1_000, 1_000_000, "Principal", scanner);
         float annualInterest = (float) MortgageUtils.inputValidator(0, 30, "Annual Interest", scanner);
         byte period = (byte) MortgageUtils.inputValidator(1, 30, "Period (in years)", scanner);
-        double mortgage = calculateMortgage(principal, annualInterest, period);
-        String monthlyPayments = NumberFormat.getCurrencyInstance(Locale.of("en", "PH")).format(mortgage);
 
-        return "Monthly Payment: " + monthlyPayments;
+        double mortgage = calculateMortgage(principal, annualInterest, period);
+        StringBuilder paymentSchedule = getPaymentSchedule(principal, annualInterest, period);
+        return "Monthly Payments: "
+                + NumberFormat.getCurrencyInstance(Locale.of("en", "PH")).format(mortgage) + "\n"
+                + "Payment Schedule \n" + "------------------------------------ \n" + paymentSchedule;
+    }
+
+    public static StringBuilder getPaymentSchedule(double principal, float annualInterest, byte period) {
+        /** formula: B = L[(1+c)^n - (1+c)^p]/[(1+c)^n-1]
+         * B = Loan Balance
+         * L = Loan amount/Principal
+         * c = monthly interest
+         * n = number of payments
+         * p = number of payments made/paid
+         * **/
+
+        final byte MONTHS_IN_A_YEAR = 12;
+        final byte PERCENT = 100;
+
+        double monthlyInterest = (annualInterest / PERCENT) / MONTHS_IN_A_YEAR;
+        int numberOfPayments = period * MONTHS_IN_A_YEAR;
+        int numberOfPaymentsMade = 1;
+        ArrayList<String> paymentSchedule = new ArrayList<>();
+        StringBuilder result = new StringBuilder();
+
+        while (numberOfPaymentsMade < numberOfPayments + 1) {
+            double loanBalance = principal * (Math.pow((1 + monthlyInterest), numberOfPayments) - Math.pow((1 + monthlyInterest), numberOfPaymentsMade)) / (Math.pow((1 + monthlyInterest), numberOfPayments) - 1);
+            paymentSchedule.add(NumberFormat.getCurrencyInstance(Locale.of("en", "PH")).format(loanBalance));
+             numberOfPaymentsMade++;
+
+        }
+
+        for (String payment : paymentSchedule) {
+            result.append(payment).append("\n");
+        }
+
+        return result;
     }
 
 
