59cef498865a525017946b171cc2b746d678c1fd
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2026-05-29 17:41:01 +0800

    Code Update: Modifed the Demo package and added new log class and logger interface

---
+++ src/main/java/dev/luderiosanchez/exercises/demo/Employee.java
@@ -0,0 +1,33 @@
+package dev.luderiosanchez.exercises.demo;
+
+public class Employee {
+    private int baseSalary;
+    private int hourlyRate;
+
+    public int calculateWage(int extraHours) {
+        return baseSalary + (hourlyRate * extraHours);
+    }
+
+    public void setBaseSalary(int baseSalary) {
+        if (baseSalary <= 0) {
+            throw new IllegalArgumentException("Base Salary cannot be 0 or negative number");
+        }
+        this.baseSalary = baseSalary;
+    }
+
+    public void setHourlyRate(int hourlyRate) {
+
+        if (hourlyRate <= 0) {
+            throw new IllegalArgumentException("Base Salary cannot be 0 or negative number");
+        }
+        this.hourlyRate = hourlyRate;
+    }
+
+    public int getBaseSalary() {
+        return baseSalary;
+    }
+
+    public int getHourlyRate() {
+        return hourlyRate;
+    }
+}
--- src/main/java/dev/luderiosanchez/exercises/demo/Main.java
+++ src/main/java/dev/luderiosanchez/exercises/demo/Main.java
@@ -1,27 +1,12 @@
 package dev.luderiosanchez.exercises.demo;
 
-import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
-        MyArrayList<String> names = new MyArrayList<>();
-        names.add("Volvo");
-        names.add("BMW");
-        names.add("Ford");
-        names.add("Mazda");
-
-        ListUtils.printAll(names);
-
-
-
-        HashSet<String> cars = new HashSet<String>();
-        cars.add("Volvo");
-        cars.add("BMW");
-        cars.add("Ford");
-        cars.add("Ford");
-        cars.add("Mazda");
-        cars.add("BMW");
-
-        System.out.println(cars);
+       var employee1 = new Employee();
+       employee1.setBaseSalary(68686);
+       employee1.setHourlyRate(30);
+       int wage = employee1.calculateWage(8);
+        System.out.println(wage);
     }
 }
