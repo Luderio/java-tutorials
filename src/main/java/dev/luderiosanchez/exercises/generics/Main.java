4ad7ba77baf294f4590810bb79a181cb2632d919
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2026-06-12 13:02:09 +0800

    Code Update: Created a new File called Utils and implemented a generic method

--- src/main/java/dev/luderiosanchez/exercises/generics/Main.java
+++ src/main/java/dev/luderiosanchez/exercises/generics/Main.java
@@ -2,10 +2,8 @@ package dev.luderiosanchez.exercises.generics;
 
 public class Main {
     public static void main(String[] args) {
-        GenericList<Integer> list = new GenericList<Integer>(10);
-        list.add(1);
-        list.get(0);
-        int number = list.get(0);
+        var bigger = Utils.max(1, 10);
+        System.out.println(bigger);
 
     }
 }
