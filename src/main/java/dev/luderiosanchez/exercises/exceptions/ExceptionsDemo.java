ebb01e83c3cfc90c7f6369e0e3e4dc994adb3a43
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2026-06-13 12:15:49 +0800

    Code Update: Refactored exceptions package

--- src/main/java/dev/luderiosanchez/exercises/exceptions/ExceptionsDemo.java
+++ src/main/java/dev/luderiosanchez/exercises/exceptions/ExceptionsDemo.java
@@ -1,9 +1,5 @@
 package dev.luderiosanchez.exercises.exceptions;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-
 public class ExceptionsDemo {
     public static void show() {
         Account account = new Account();
