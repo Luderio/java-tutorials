4c46b6d5cb02e427f02572ada050d6c95593ccf7
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2026-06-15 17:36:18 +0800

    Code Update: Created a new package called comparable_sort

--- src/main/java/dev/luderiosanchez/exercises/collections/Main.java
+++ src/main/java/dev/luderiosanchez/exercises/collections/Main.java
@@ -1,7 +1,18 @@
 package dev.luderiosanchez.exercises.collections;
 
+import dev.luderiosanchez.exercises.collections.comparable_sort.Customer;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        CollectionsDemo.show();
+        List<Customer> customers = new ArrayList<>();
+        customers.add(new Customer("Ian"));
+        customers.add(new Customer("Chezzy"));
+        customers.add(new Customer("Hero"));
+        Collections.sort(customers);
+        System.out.println(customers);
     }
 }
