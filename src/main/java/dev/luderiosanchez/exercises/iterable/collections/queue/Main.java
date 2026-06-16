56c9b7af3135244aa26080e4bdda8d9186574f47
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2026-06-16 20:00:31 +0800

    Code Update: Created a new package called queue and explored how Queue worked in Java

---
+++ src/main/java/dev/luderiosanchez/exercises/iterable/collections/queue/Main.java
@@ -0,0 +1,7 @@
+package dev.luderiosanchez.exercises.iterable.collections.queue;
+
+public class Main {
+    public static void main(String[] args) {
+        QueueDemo.show();
+    }
+}
---
+++ src/main/java/dev/luderiosanchez/exercises/iterable/collections/queue/QueueDemo.java
@@ -0,0 +1,19 @@
+package dev.luderiosanchez.exercises.iterable.collections.queue;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class QueueDemo {
+    public  static void show() {
+        Queue<String> queue = new ArrayDeque<>();
+        queue.add("c");
+        queue.add("a");
+        queue.add("b");
+
+        var front = queue.peek();
+        queue.remove();
+        var newFront = queue.peek();
+        System.out.println(front);
+        System.out.println(newFront);
+    }
+}
