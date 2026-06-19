220186a9b86e874da7e620550acff88ac0e4bcd7
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2026-06-19 22:43:05 +0800

    Code Update

--- src/main/java/dev/luderiosanchez/exercises/lambdas/LambdasDemo.java
+++ src/main/java/dev/luderiosanchez/exercises/lambdas/LambdasDemo.java
@@ -1,16 +1,14 @@
 package dev.luderiosanchez.exercises.lambdas;
 
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
+import java.util.function.*;
 
 public class LambdasDemo {
     public static void show() {
-        Predicate<String> isLongerThan5 = str -> str.length() > 5;
-        var result = isLongerThan5.test("Sky");
+        BinaryOperator<Integer> add = (a, b) -> a + b;;
+        Function<Integer, Integer> squared = x -> x * x;
 
+        var result = add.andThen(squared).apply(2, 5);
         System.out.println(result);
     }
 
