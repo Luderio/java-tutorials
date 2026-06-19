739688903544e02fcbb2110da949925aedb92c6b
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2026-06-19 18:15:09 +0800

    Code Update

--- src/main/java/dev/luderiosanchez/exercises/lambdas/LambdasDemo.java
+++ src/main/java/dev/luderiosanchez/exercises/lambdas/LambdasDemo.java
@@ -2,15 +2,12 @@ package dev.luderiosanchez.exercises.lambdas;
 
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class LambdasDemo {
     public static void show() {
-        List<String> list = List.of("Chezzy", "Ian", "Hero");
-
-        Consumer<String> print = item -> System.out.println(item);
-        Consumer<String> printUpperCase = item -> System.out.println(item.toUpperCase());
-        list.forEach(print.andThen(printUpperCase).andThen(print));
-
+        Supplier<Double> getRandom = () ->  Math.random();
+        System.out.println(getRandom.get());
     }
 
 
