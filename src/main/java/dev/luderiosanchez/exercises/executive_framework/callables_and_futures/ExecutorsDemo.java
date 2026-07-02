62703dbd6ba7a675456e7fbde8286c9d56c70024
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2026-07-02 09:16:29 +0800

    Code Update: Saving code changes for the Executive Framework section of the course: Lesson 4 - Callables and Futures

--- src/main/java/dev/luderiosanchez/exercises/executive_framework/callables_and_futures/ExecutorsDemo.java
+++ src/main/java/dev/luderiosanchez/exercises/executive_framework/callables_and_futures/ExecutorsDemo.java
@@ -7,6 +7,13 @@ import java.util.concurrent.Executors;
 public class ExecutorsDemo {
     public static void show() {
         try (ExecutorService executor = Executors.newFixedThreadPool(2)) {
+            /*
+            - An executor.submit() with a lambda function similar to the Callable interface.
+            - Returns a Future<Integer> type. a value that will execute in the future and not immediately.
+            - the .submit() method will hand of the specified tasks to a separate worker thread, and will free up the main thread tp work on other stuff.
+            - future.get() will get the returned value from the worker thread.
+            - If the worker thread is not yet done with the task, the main thread will be blocked until the worker thread is finished doing the task.
+            */
             var future = executor.submit(() -> {
                 LongTask.simulate();
                 return 1;
