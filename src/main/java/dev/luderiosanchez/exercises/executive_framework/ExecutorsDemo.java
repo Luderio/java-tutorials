2fa2a42ea91bbddaccdecddea99e85eec75be544
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2026-07-02 09:06:43 +0800

    Code Update: Saving code changes for the Executive Framework section of the course: Lesson 4 - Callables and Futures

--- src/main/java/dev/luderiosanchez/exercises/executive_framework/ExecutorsDemo.java
+++ src/main/java/dev/luderiosanchez/exercises/executive_framework/ExecutorsDemo.java
@@ -1,14 +1,21 @@
 package dev.luderiosanchez.exercises.executive_framework;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ExecutorsDemo {
     public static void show() {
         try (ExecutorService executor = Executors.newFixedThreadPool(2)) {
-            executor.submit(() -> {
-                System.out.println(Thread.currentThread().getName());
+            var future = executor.submit(() -> {
+                LongTask.simulate();
+                return 1;
             });
+            System.out.println("Do more work");
+            var result = future.get();
+            System.out.println(result);
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 }
