c75c068dca7b4111acf3f6db377c4c79846a4ca1
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2026-07-02 08:33:59 +0800

    Code Update: Saving code changes for the Executive Framework section of the course: Lesson 3 - Executors

---
+++ src/main/java/dev/luderiosanchez/exercises/executive_framework/ExecutorsDemo.java
@@ -0,0 +1,14 @@
+package dev.luderiosanchez.exercises.executive_framework;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecutorsDemo {
+    public static void show() {
+        try (ExecutorService executor = Executors.newFixedThreadPool(2)) {
+            executor.submit(() -> {
+                System.out.println(Thread.currentThread().getName());
+            });
+        }
+    }
+}
